3ebc394636cf094dae2dabde17c6b38adbb3c989
Nick Black <dank@qemfd.net>
2009-11-02 00:17:00 -0500

    drop ssl code from libdank, per bug 25 (http://dank.qemfd.net/bugzilla/show_bug.cgi?id=25) this is now libtorque territory

--- libdank/modules/ssl/crl.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBDANK_MODULES_SSL_CRL
-#define LIBDANK_MODULES_SSL_CRL
-
-#include <openssl/ssl.h>
-
-X509_CRL *open_x509_crl_pemfile(const X509_STORE_CTX *,const char *);
-int is_x509_cert_revoked(const X509_CRL *,const X509 *);
-
-#endif
